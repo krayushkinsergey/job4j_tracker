2ec6f9666f930f8010e4b450f76fea0922e36d7c
Sergey Krayushkin <Krayushkin.sergey@gmail.com>
2023-05-31 20:25:59 +0300

    5.0. Приведение типов. Повышение типа и понижение типа. [#181322  [#181322]]

---
+++ src/main/java/ru/job4j/oop/College.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class College {
+    public static void main(String[] args) {
+        Freshman freshmen = new Freshman();
+        Student student = freshmen;
+        Object obj = freshmen;
+    }
+}
