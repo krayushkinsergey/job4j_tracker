0dae8f778062e78e8df490b530e9ad0ed4cfb655
Sergey Krayushkin <Krayushkin.sergey@gmail.com>
2023-06-25 13:31:19 +0300

    12.0. Написать тесты на StartUI. [#785  [#785 #424125]]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -25,7 +25,7 @@ class StartUITest {
         Item item = tracker.add(new Item("Replaced item"));
         String replacedName = "New item name";
         Input in = new StubInput(
-                new String[] {"0", "1", replacedName, "1"}
+                new String[] {"0", String.valueOf(item.getId()), replacedName, "1"}
         );
         Output out = new ConsoleOutput();
         UserAction[] actions = {
@@ -41,7 +41,7 @@ class StartUITest {
         Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("Deleted item"));
         Input in = new StubInput(
-                new String[] {"0", "1", "1"}
+                new String[] {"0", String.valueOf(item.getId()), "1"}
         );
         Output out = new ConsoleOutput();
         UserAction[] actions = {
