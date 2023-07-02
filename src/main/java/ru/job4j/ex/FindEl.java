73246ae72114bf63881e823e446c58a26212451a
Sergey Krayushkin <Krayushkin.sergey@gmail.com>
2023-07-02 17:46:58 +0300

    0.4. Пользовательские исключения. [#219367  [#219367]]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,32 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        /* цикл fori, поскольку нам надо найти индекс искомого элемента в массиве */
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+                break;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Элемент " + key + " не найден");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] values = {"Элемент1", "Элемент2", "Элемент3"};
+
+        try {
+            int findEl = FindEl.indexOf(values, "Элемент2");
+            System.out.println("Найденный индекс элемента " + findEl);
+
+            findEl = FindEl.indexOf(values, "Элемент 3");
+            System.out.println("Найденный индекс элемента " + findEl);
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
