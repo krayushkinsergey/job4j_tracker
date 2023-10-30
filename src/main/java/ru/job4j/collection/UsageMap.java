acc8284fbff744bd5d780ed68f2092895751b687
Sergey Krayushkin <Krayushkin.sergey@gmail.com>
2023-10-30 06:36:32 +0300

    0. Map, HashMap [#212652 #456350]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("krayushkin.sergey@gmail.com", "Краюшкин Сергей");
+        map.put("krayushkin.sergey@gmail.com", "Краюшкин Сергей Александрович");
+        map.put("test@gmail.com", "test_google");
+        map.put("test@yandex.ru", "test_yandex");
         for (String key: map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
